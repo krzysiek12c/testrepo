40e62e1b6b0f7512fce576e741bc6704292503c1
krzysiek12c <138445409+krzysiek12c@users.noreply.github.com>
2023-07-03 10:44:49 +0200

    Add files via upload

---
+++ DLP Test Python.py
@@ -0,0 +1,1165 @@
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+
+
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+valuesprint("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+valuesprint("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
+print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")print("Hello World")
+
+#Variables
+name = "Chris"
+age = 30
+print(name)
+print(age)
+
+#While Loop
+i = 1
+while i < 3:
+  print(i)
+  i += 1
+  
+#For Loop
+names = ["Chris", "Harry", "Tom"]
+for name in names:
+  print(name)
+print("Hello World")
